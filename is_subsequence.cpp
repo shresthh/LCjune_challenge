40470d6ee59e802d24fb2da231bfce7af54a1d9c
shresthh <sumuandmishra@gmail.com>
2020-06-10 00:25:11 +0530

    this code does not contain main function

---
+++ is_subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int tpos =0;
+        for(int i=0;s[i]!='\0';i++){
+            while(t[tpos] && t[tpos]!=s[i]){
+                tpos+=1;
+            }
+            if(t[tpos]=='\0'){
+                return false;
+            }
+            tpos+=1;
+        }
+        return true;
+        
+    }
+};
